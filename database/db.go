cc1112f811ad16f3d583f171048a3e2906f90d86
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-11-26 23:58:17 +0700

    chore: maintain the development/local way to connect db

--- database/db.go
+++ database/db.go
@@ -14,9 +14,14 @@ var DB *gorm.DB
 func LoadDB() {
 	cfg := config.GetConfig()
 
-	SOCKET_DIR := "/cloudsql"
-
-	dsn := fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", cfg.DB_USER, cfg.DB_PASSWORD, SOCKET_DIR, cfg.DB_HOST, cfg.DB_PORT, cfg.DB_NAME)
+	var dsn string
+	if cfg.APP_ENV == "development" {
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", cfg.DB_USER, cfg.DB_PASSWORD, cfg.DB_HOST, cfg.DB_PORT, cfg.DB_NAME)
+	} else {
+		SOCKET_DIR := "/cloudsql"
+		dsn = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			cfg.DB_USER, cfg.DB_PASSWORD, SOCKET_DIR, cfg.DB_HOST, cfg.DB_NAME)
+	}
 
 	log.Println(dsn)
 
