8105aea5375a2a55f6706d0ef043d5b00a33b8a3
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-12-03 23:49:57 +0700

    chore: add condition on prod

--- utils/gcs.go
+++ utils/gcs.go
@@ -17,11 +17,23 @@ func InitGCS() {
 	cfg := config.GetConfig()
 	ctx := context.Background()
 
-	client, err := storage.NewClient(ctx, option.WithCredentialsFile(cfg.GOOGLE_APPLICATION_CREDENTIALS))
-	if err != nil {
-		log.Fatalf("Failed to initialize GCS client: %v", err)
+	var client *storage.Client
+	var err error
+
+	// Check if the environment is "development"
+	if cfg.APP_ENV == "development" {
+		client, err = storage.NewClient(ctx, option.WithCredentialsFile(cfg.GOOGLE_APPLICATION_CREDENTIALS))
+		if err != nil {
+			log.Fatalf("Failed to initialize GCS client with credentials file: %v", err)
+		}
+		log.Println("GCS client initialized successfully with explicit credentials (development mode)")
+	} else {
+		client, err = storage.NewClient(ctx)
+		if err != nil {
+			log.Fatalf("Failed to initialize GCS client with default credentials: %v", err)
+		}
+		log.Println("GCS client initialized successfully with default credentials (production mode)")
 	}
 
 	GCSClient = client
-	log.Println("GCS client initialized successfully with explicit credentials")
 }
